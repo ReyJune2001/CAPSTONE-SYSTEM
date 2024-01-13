5adc9e9ec352a60f965834884fed47803426385b
Rey June <ucabreyjune2001@gmail.com>
2024-01-13 10:47:25 +0800

    16th commit

--- MCC/login.php
+++ MCC/login.php
@@ -6,28 +6,28 @@ $invalid=0;
 
 if ($_SERVER['REQUEST_METHOD']=='POST'){
   include 'connect.php';
-  $Name = $_POST['Name'];
+  $Username = $_POST['Username'];
   $Password = $_POST['Password'];
   
  // Check if the login credentials match an admin account
- $admin_sql = "SELECT * FROM `user_admin_account` WHERE Name = '$Name' AND password = '$Password' AND Level = 'admin'";
+ $admin_sql = "SELECT * FROM `operator_admin_account` WHERE Username = '$Username' AND password = '$Password' AND Level = 'Admin'";
  $admin_result = mysqli_query($con, $admin_sql);
 
  // Check if the login credentials match a user account
- $user_sql = "SELECT * FROM `user_admin_account` WHERE Name = '$Name' AND password = '$Password' AND Level = 'user'";
+ $user_sql = "SELECT * FROM `operator_admin_account` WHERE Username = '$Username' AND password = '$Password' AND Level = 'Operator'";
  $user_result = mysqli_query($con, $user_sql);
 
 
  if ($admin_result && mysqli_num_rows($admin_result) > 0) {
         // Admin login successful
         session_start();
-        $_SESSION['Name'] = $Name;
+        $_SESSION['Username'] = $Username;
         $login_admin = 1;
         header('location:dataEntry.php');
     } elseif ($user_result && mysqli_num_rows($user_result) > 0) {
         // User login successful
         session_start();
-        $_SESSION['Name'] = $Name;
+        $_SESSION['Username'] = $Username;
         $login_user = 1;
         header('location:mobile_dataEntry.php');
     } else {
@@ -58,14 +58,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
         font-family: 'Noto+Serif+Makasar';
       }
 
-
-/*background image*/ 
-.gradient-custom-3 {
-  background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(IMAGES/background2.jpg);
-  background-size: cover;
-  
-}
-
 /*logo*/
 
 .logo{
@@ -74,38 +66,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
             left: 20px; /* Adjust the left position as needed */
         
 }
-footer {
-    
-    color: black;
-    padding-top:9px;
-   
-    text-align: center;
-    
-}
-
-.card-body {
-    background-color: rgba(255, 255, 255, 0.5); /* Adjust the alpha value for transparency */
-    border-radius: 50px;
-    
-  }
-  /*to change the color of my <p>*/
-  .text-center.text-muted.mt-2 {
-    color: white !important; /* Set the desired text color */
-  }
-
-  .text-body {
-        color: white !important; /* Set the desired text color */
-      }
-
-  h1 {
-        color:skyblue !important; /* Set the desired text color */
-        margin-top:70px;
-      }
-  
-  h2 {
-        color:white !important; /* Set the desired text color */
-       
-  }
 
 /*Input fields*/
 .form-outline{
@@ -124,9 +84,9 @@ footer {
 
 /*Login button*/
 .btn{
-    width: 300px;
-    height: 45px; 
-    font-size: 19px;
+    width:100%;
+    height:45px;
+    font-size: 23px;
     border-radius:50px;
 }
 
@@ -144,6 +104,65 @@ footer {
     border-top: 1px solid #2c3e50; /* Border color for the footer */
   }
 
+
+  body{
+            height: 100vh;
+            background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(IMAGES/background2.jpg);
+            background-size: cover;
+            font-family: sans-serif;
+            font-size: 24PX;
+            display: flex;
+            flex-direction: column;
+             
+        }
+
+
+  header{
+            background-color: transparent;
+            text-align: center;
+            padding: 2em 0 2em 0;
+            color:white;
+            font-size:50px;
+            margin-top:40px;
+            padding-bottom:0px;
+        }
+        .left{
+            background-color:transparent;
+            padding: 3em 0 3em 0;
+            flex: 1 1 100px;
+            margin-left: auto;
+ 
+        }
+        .main{
+            display: flex;
+            flex: 1;
+        }
+        main{
+            background-color: transparent;
+            padding: 3em 0 3em 0;
+            flex: 1 1 150px;
+            color:white;
+            padding-top:40px;
+
+        }
+        .right{
+            background-color: transparent;
+            padding: 3em 0 3em 0;
+            flex: 1 1 100px;
+            margin-right: auto;
+
+        }
+        footer{
+            background-color: transparent;
+            text-align:center;
+            padding: 1em 0 1em 0;
+            color:white;
+
+        }
+
+        .wrapper{
+          background-color:black;
+        }
     </style>
   </head>
   <body>
@@ -182,30 +201,25 @@ footer {
     </div>
 </div>
 
-<section class="vh-100 bg-image">
-    <!--For background image-->
-    
-  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-   
-  
-  <!--For Logo-->
+  <header>
+    <!--For Logo-->
   <img src="IMAGES/logo.jpg" alt="Registration Image" width="100" height="50" class="logo">
   
-  
-    <div class="container h-100 pt-4 pb-0">
-    <h1 class="text-center pt-5 mb-5">PAINT AND ACETATE YIELD MONITORING SYSTEM</h1>
-
-      <div class="row d-flex justify-content-center align-items-center h-90">
+    PAINT AND ACETATE YIELD MONITORING SYSTEM
+    <br>
+    <br>
+    <h1 class="text-center mb-5">Good Day</h1>
+  </header>
+    <div class="main">
+        <aside class="left"></aside>
       
-
-      <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-      <h2 class="text-center mb-5">Good Day</h2>
-          <div>
-              <form action="login.php" method="post">
-              <div class="form-outline">
+       <main>
+       <form action="login.php" method="post">
+            
+                <div class="form-outline">
                   <div class="input-group">
                   <span class="input-group-text"><i class="fas fa-user"></i></span>
-                  <input type="text" class="form-control form-control-lg" placeholder="Username" name="Name" required />
+                  <input type="text" class="form-control form-control-lg" placeholder="Username" name="Username" required />
                   </div>
                 </div>
 
@@ -216,24 +230,18 @@ footer {
                  <input type="password" class="form-control form-control-lg" placeholder="Password" name="Password" required />
                  </div>
                </div>
-
-                <div class="d-flex justify-content-center">
-                  <button type="submit" class="btn btn-primary btn-lg text-body">Login</button>
-                </div>
-                
-                <br>
-                <p class="text-center text-muted mt-2">You don't have an account yet? <a href="index.php" class="fw-bold text-body"><u>Sign up here</u></a></p>
-              </form>
-
-            
-          </div>
-          <footer style="color:white;">
-            Copyright © All rights reserved ~ MCC Internship 2023
-          </footer>
-        </div>
-      </div>
+               
+               <div class="form-outline">
+              <div class="text-center pt-1 mb-5 pb-1">
+                 <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">
+                 Login
+                 </button>
+              </div>
+              </div>
+               </form>
+       </main>
+       <aside class="right"></aside>
     </div>
-  </div>
-</section>
+    <footer>Copyright © All rights reserved ~ MCC Interns 2023</footer>
 </body>
 </html>
